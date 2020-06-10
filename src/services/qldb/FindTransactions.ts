e1872da2e930ac4c52f5ebedac64ea00c9a6dd2d
WalczakPaulo <walczakp007@gmail.com>
2020-06-10 12:09:51 +0200

    error handling

--- src/services/qldb/FindTransactions.ts
+++ src/services/qldb/FindTransactions.ts
@@ -16,17 +16,21 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { createQldbWriter, QldbSession, QldbWriter, Result, TransactionExecutor } from "amazon-qldb-driver-nodejs";
+import {
+  createQldbWriter, QldbSession, QldbWriter, Result, TransactionExecutor
+} from 'amazon-qldb-driver-nodejs';
 import { Request, Response, RequestHandler } from 'express';
 
-import { closeQldbSession, createQldbSession } from "./ConnectToLedger";
-import { AD_DATA_TRANSACTIONS } from "./model/SampleData";
-import { AD_DATA_TABLE_NAME } from "./qldb/Constants";
-import { error, log } from "./qldb/LogUtil";
-import { getDocumentId, writeValueAsIon } from "./qldb/Util";
-import { prettyPrintResultList } from "./ScanTable";
-import { decodeUtf8, makePrettyWriter, Reader, Writer } from "ion-js";
-import { ionToJSON } from "ion-to-json";
+import {
+  decodeUtf8, makePrettyWriter, Reader, Writer
+} from 'ion-js';
+import { ionToJSON } from 'ion-to-json';
+import { closeQldbSession, createQldbSession } from './ConnectToLedger';
+import { AD_DATA_TRANSACTIONS } from './model/SampleData';
+import { AD_DATA_TABLE_NAME } from './qldb/Constants';
+import { error, log } from './qldb/LogUtil';
+import { getDocumentId, writeValueAsIon } from './qldb/Util';
+import { prettyPrintResultList } from './ScanTable';
 import { Transaction } from './model/Transaction';
 
 /**
@@ -35,84 +39,82 @@ import { Transaction } from './model/Transaction';
  * @param govId The owner's government ID.
  * @returns Promise which fulfills with void.
  */
- export function getJsonResult(resultList: Reader[]): Transaction[] {
-     return ionToJSON(resultList);
- }
+export function getJsonResult(resultList: Reader[]): Transaction[] {
+  return ionToJSON(resultList);
+}
 
 export async function findTransactionsForCompany(txn: TransactionExecutor, company: string): Promise<Transaction[]> {
-    const query: string = `SELECT * FROM AdData WHERE Company = ?`;
-    const qldbWriter: QldbWriter = createQldbWriter();
-    writeValueAsIon(company, qldbWriter);
+  const query: string = 'SELECT * FROM AdData WHERE Company = ?';
+  const qldbWriter: QldbWriter = createQldbWriter();
+  writeValueAsIon(company, qldbWriter);
 
-    const resultList = await txn.executeInline(query, [qldbWriter]).then((result: Result) => {
-        const resultList: Reader[] = result.getResultList();
-        log(`List of transactions for company with Name: ${company}`);
-        //prettyPrintResultList(resultList);
-        return resultList;
-    });
-    return getJsonResult(resultList);
+  const resultList = await txn.executeInline(query, [qldbWriter]).then((result: Result) => {
+    const resultList: Reader[] = result.getResultList();
+    log(`List of transactions for company with Name: ${company}`);
+    // prettyPrintResultList(resultList);
+    return resultList;
+  });
+  return getJsonResult(resultList);
 }
 
 export async function getTransactions(txn: TransactionExecutor): Promise<Transaction[]> {
-    const query: string = `SELECT id, company, inEth, amount FROM AdData BY id`;
+  const query: string = 'SELECT id, company, inEth, amount FROM AdData BY id';
 
-    const resultList = await txn.executeInline(query, []).then((result: Result) => {
-        const resultList: Reader[] = result.getResultList();
-        log(`List of transactions`);
-        //prettyPrintResultList(resultList);
-        return resultList;
-    });
-    return getJsonResult(resultList);
+  const resultList = await txn.executeInline(query, []).then((result: Result) => {
+    const resultList: Reader[] = result.getResultList();
+    log('List of transactions');
+    // prettyPrintResultList(resultList);
+    return resultList;
+  });
+  return getJsonResult(resultList);
 }
 
 export const getAllTransactionsHandler: RequestHandler = async (req: Request, res: Response) => {
   let session: QldbSession;
   try {
-      session = await createQldbSession();
-      await session.executeLambda(async (txn) => {
-          const response = await getTransactions(txn);
-          res.send(response).status(200);
-      }, () => log("Retrying due to OCC conflict..."));
-  } catch(err) {
-    res.sendStatus(err.statusCode || 500);
-  } finally {
+    session = await createQldbSession();
+    await session.executeLambda(async (txn) => {
+      const response = await getTransactions(txn);
+      res.send(response).status(200);
+    }, () => log('Retrying due to OCC conflict...'));
+  }  finally {
     closeQldbSession(session);
   }
-}
+};
 
 export const findTransactionsHandler: RequestHandler = async (req: Request, res: Response) => {
   let session: QldbSession;
   try {
-      session = await createQldbSession();
-      throw "BIg";
-      throw 500;
-      const vla = 5/ 0;
-      await session.executeLambda(async (txn) => {
-          const response = await findTransactionsForCompany(txn, req.query.company);
-          res.send(response).status(200);
-      }, () => log("Retrying due to OCC conflict..."));
+    session = await createQldbSession();
+    throw 'BIg';
+    throw 500;
+    const vla = 5 / 0;
+    await session.executeLambda(async (txn) => {
+      const response = await findTransactionsForCompany(txn, req.query.company);
+      res.send(response).status(200);
+    }, () => log('Retrying due to OCC conflict...'));
   } finally {
-      closeQldbSession(session);
+    closeQldbSession(session);
   }
-}
+};
 /**
  * Find all vehicles registered under a person.
  * @returns Promise which fulfills with void.
  */
-var main = async function(): Promise<void> {
-    let session: QldbSession;
-    try {
-        session = await createQldbSession();
-        await session.executeLambda(async (txn) => {
-            await findTransactionsForCompany(txn, AD_DATA_TRANSACTIONS[0].company);
-        }, () => log("Retrying due to OCC conflict..."));
-    } catch (e) {
-        error(`Error getting vehicles for owner: ${e}`);
-    } finally {
-        closeQldbSession(session);
-    }
-}
+const main = async function (): Promise<void> {
+  let session: QldbSession;
+  try {
+    session = await createQldbSession();
+    await session.executeLambda(async (txn) => {
+      await findTransactionsForCompany(txn, AD_DATA_TRANSACTIONS[0].company);
+    }, () => log('Retrying due to OCC conflict...'));
+  } catch (e) {
+    error(`Error getting vehicles for owner: ${e}`);
+  } finally {
+    closeQldbSession(session);
+  }
+};
 
 if (require.main === module) {
-    main();
+  main();
 }
